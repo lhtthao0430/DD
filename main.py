72191567b5912ba97de35f7b6f87d8de873e9279
lhtthao0430 <42653530+lhtthao0430@users.noreply.github.com>
2019-12-21 22:55:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -32,23 +32,27 @@ def NaiveDecode(infile):
             if columnIndex[j] == '0':
                 if M[i] & (1 << (n - j - 1)) != 0:
                     M[i] = M[i] ^ (1 << (n - j - 1))
-    return M
+    return M, n
 
 if __name__ == "__main__":
-    X = NaiveDecode("input.txt")
+    X, n = NaiveDecode("input.txt")
+    # for i in X:
+    #     print(bin(i)[2:].zfill(8))
 
     # DD
     K = []
     for i in X:
         if i != 0 and math.ceil(math.log(i, 2)) == math.floor(math.log(i, 2)): # check if i is power of 2 or there is only one number 1 in a test
-            K.append(int(math.log(i, 2)))
+            K.append(n - int(math.log(i, 2)))
     print("DD = ", K)
 
      # COMP
     K = set()
     for i in X:
         if i != 0:
-            K.add(int(math.log(i, 2)))
+            for j in range(0, n):
+                if i & (1 << j) != 0:
+                    K.add(n - int(math.log(1 << j, 2)))
     K = list(K)
     print("COMP = ", K)
     pass
